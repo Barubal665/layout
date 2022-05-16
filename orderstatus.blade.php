cbf33353c7ac6494fd36711ccdcd9bd3c770884a
Barubal665 <89123787+Barubal665@users.noreply.github.com>
2022-05-16 09:35:55 +0800

    Add files via upload

---
+++ orderstatus.blade.php
@@ -0,0 +1,77 @@
+@extends('layout')
+@section('content')
+
+<div id="rightSideWrapper">
+
+<div class="ContentBox">
+
+  <main>
+    <table>
+      <thead>
+        <tr>
+          <th>Order no.</th>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Receive</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+        <tr>
+          <td data-column="First Name">Sample</td>
+          <td data-column="Last Name">Sample</td>
+          <td data-column="Job Title">Sample</td>
+          <td data-column="Twitter">Sample</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </main>
+
+
+
+</div>
+
+</div>
+@endsection
