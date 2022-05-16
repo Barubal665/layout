ec67260d98069763c886f438b77ddf9a56231b97
Barubal665 <89123787+Barubal665@users.noreply.github.com>
2022-05-16 09:27:49 +0800

    Add files via upload

---
+++ orderlist.blade.php
@@ -0,0 +1,230 @@
+@extends('layout')
+@section('content')
+		<div id="rightSideWrapper">
+			
+			<div class="ContentBox">
+      <main> 
+          <div class="Card1">
+        
+            <div class="description">
+              <div class="line">
+                <h1 class="product_name">Card Title</h1>
+                <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+              </div>
+              <h2>sample date</h2>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <a href="//s.codepen.io/ImagineAlex">Read More</a>
+            </div>
+          </div>
+          <div class="Card1">
+          
+            <div class="description">
+              <div class="line">
+                <h1 class="product_name">Card Title</h1>
+                <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+              </div>
+              <h2>sample date</h2>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <a href="//s.codepen.io/ImagineAlex">Read More</a>
+            </div>
+          </div>
+          <div class="Card1">
+          
+            <div class="description">
+              <div class="line">
+                <h1 class="product_name">Card Title</h1>
+                <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+              </div>
+              <h2>sample date</h2>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <a href="//s.codepen.io/ImagineAlex">Read More</a>
+            </div>
+          </div>
+          <div class="Card1">
+          
+            <div class="description">
+              <div class="line">
+                <h1 class="product_name">Card Title</h1>
+                <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+              </div>
+              <h2>sample date</h2>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <a href="//s.codepen.io/ImagineAlex">Read More</a>
+            </div>
+          </div>
+          <div class="Card1">
+          
+            <div class="description">
+              <div class="line">
+                <h1 class="product_name">Card Title</h1>
+                <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+              </div>
+              <h2>sample date</h2>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <p class="summary">Sample text.</p>
+              <a href="//s.codepen.io/ImagineAlex">Read More</a>
+            </div>
+          </div>      
+        
+        
+        
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+        <div class="Card1">
+        
+          <div class="description">
+            <div class="line">
+              <h1 class="product_name">Card Title</h1>
+              <h1 class="product_price"><i class='bx bx-user-circle'></i></h1>
+            </div>
+            <h2>sample date</h2>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <p class="summary">Sample text.</p>
+            <a href="//s.codepen.io/ImagineAlex">Read More</a>
+          </div>
+        </div>
+                   
+				</main>
+
+			
+
+			</div>
+			
+		</div>
+
+@endsection
