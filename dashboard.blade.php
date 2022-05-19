d06bc12e2792de4c9d90b05ebfb3119efda817fd
Barubal665 <89123787+Barubal665@users.noreply.github.com>
2022-05-19 09:04:53 +0800

    Add files via upload

---
+++ dashboard.blade.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+</head>
+<body>
+    <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <img src="/css/images/logowhite.png" alt="">
+        </div>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="" class="active"><span class="las la-chart-line"></span>
+                <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-list icon"></span>
+                <span>Order List</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-shopping-bag icon"></span>
+                <span>Inventory</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-check-circle"></span>
+                <span>Order Status</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-hamburger icon "></span>
+                <span>Product</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-users icon "></span>
+                <span>User</span></a>
+                </li>
+    
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <header>
+            <div class="header-title">
+              <h2>
+              <label for="nav-toggle">
+                    <span class="las la-bars"></span>
+                </label>
+                Dashboard
+              </h2>
+                </div>
+              <div class="search-wrapper">
+                  <span class="las la-search"></span>
+                  <input type="search" placeholder = "Search here"/>
+              </div>
+            
+               <div class="user-wrapper">
+                   <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                   <div>
+               <h4>Pisopay</h4>
+               <small>Admin</small>
+                </div>
+                </div>
+
+        </header>
+
+        <main>
+
+            <div class="cards">
+                <div class="card-single">
+                    <div>
+                         <h1>2</h1>
+                         <span>Users</span>
+                    </div>
+                    <div>
+                        <span class="las la-users"></span>
+                    </div>
+                </div>
+                <div class="card-single">
+                    <div>
+                         <h1>2</h1>
+                         <span>Projects</span>
+                    </div>
+                    <div>
+                        <span class="las la-clipboard"></span>
+            </div>
+            </div>
+                <div class="card-single">
+                    <div>
+                         <h1>2</h1>
+                         <span>Orders</span>
+                    </div>
+                    <div>
+                        <span class="las la-shopping-bag"></span>
+                        </div>
+                        </div>
+                <div class="card-single">
+                    <div>
+                         <h1>2</h1>
+                         <span>Income</span>
+                    </div>
+                    <div>
+                        <span class="las la-wallet"></span>
+                        </div>
+                        </div>
+                            </div>
+
+
+                        <div class="recent-grid">
+                            <div class="projects">
+                                <div class="card">
+                                <div class="card-header">
+                                    <h3>Recent Projects</h3>
+
+                                    <button>See all <span class="las la-arrow-right">
+                                    </span></button>
+                                </div>
+
+                                <div class="card-body">
+                                <div class="table-responsive">
+                                    <table width="100%">
+                                        <thead>
+                                            <tr>
+                                                    <td>Project Title</td>
+                                                    <td>Department</td>
+                                                    <td>Status</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>asdasdas</td>
+                                                <td>asdaasdasdasd</td>
+                                                <td>
+                                                    <span class="status purple"></span>
+                                                review
+                                            </td> 
+                                            </tr>
+                                            <tr>
+                                                <td>asdsadasdasd</td>
+                                                <td>asdaasdasd</td>
+                                                <td>
+                                                    <span class="status pink"></span>
+                                                inporgress
+                                            </td>
+                                            </tr>
+                                            <tr>
+                                                <td>asdsadsadsadsa</td>
+                                                <td>asdaasdsa</td>
+                                                <td>
+                                                    <span class="status orange"></span>
+                                                pending
+                                            </td>
+                                            </tr>
+                                            <tr>
+                                                <td>asdasdas</td>
+                                                <td>asdaasdasdasd</td>
+                                                <td>
+                                                    <span class="status purple"></span>
+                                                review
+                                            </td> 
+                                            </tr>
+                                            <tr>
+                                                <td>asdsadasdasd</td>
+                                                <td>asdaasdasd</td>
+                                                <td>
+                                                    <span class="status pink"></span>
+                                                inporgress
+                                            </td>
+                                            </tr>
+                                            <tr>
+                                                <td>asdsadsadsadsa</td>
+                                                <td>asdaasdsa</td>
+                                                <td>
+                                                    <span class="status orange"></span>
+                                                pending
+                                            </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                                        
+                                
+
+                            <div class="users">
+                                <div class="card">
+                                <div class="card-header">
+                                    <h3>New User</h3>
+
+                                    <button>See all <span class="las la-arrow-right">
+                                    </span></button>
+                                </div>
+
+                                <div class="card-body"> 
+                                    <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                      
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        <div class="customer">
+                                        <div class="info">
+                                        <img src="/css/images/pisopay.png" width="40px" height="40px" alt="">
+                                        <div>
+                                            <h4>topherski</h4>
+                                            <small>pogi</small>
+                                        </div>
+                                        </div>
+                                <div class="contacts">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                        </div>
+                        </div>
+
+                        </div>
+        </main>
+    </div>
+</body>
+</html>
